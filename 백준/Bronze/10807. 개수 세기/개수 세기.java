47f6c6ec8642f0a01d781378f5a4dd1903edb835
Sumin Park <126743151+330sum@users.noreply.github.com>
2023-04-21 02:35:16 +0900

    [Bronze V] Title: 개수 세기, Time: 216 ms, Memory: 17824 KB -BaekjoonHub

---
+++ 백준/Bronze/10807. 개수 세기/개수 세기.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int findNum = sc.nextInt();
+
+        int count = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == findNum) {
+                count++;
+             
+            }
+        }
+        System.out.println(count);
+    }
+}
