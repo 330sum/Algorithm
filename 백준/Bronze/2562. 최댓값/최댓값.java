eb8eb68e102b5e3b4de324f52500d43c926bba6b
Sumin Park <126743151+330sum@users.noreply.github.com>
2023-04-22 09:42:20 +0900

    [Bronze III] Title: 최댓값, Time: 208 ms, Memory: 17700 KB -BaekjoonHub

---
+++ 백준/Bronze/2562. 최댓값/최댓값.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int[] arr = new int[9];
+        for (int i = 0; i < 9; i++) {
+            arr[i] = sc.nextInt();
+        }
+        
+        int max = arr[0];
+        for (int i = 0; i < arr.length; i++) {
+            if (max < arr[i]) max = arr[i];
+        }
+        System.out.println(max);
+        
+        for (int i = 0; i < arr.length; i++) {
+            if (max == arr[i])
+                System.out.println(i + 1);
+        }
+
+    }
+}
