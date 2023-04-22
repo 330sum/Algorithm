8a406af7d637a79d19cca5fd817633bd8d2e0dce
Sumin Park <126743151+330sum@users.noreply.github.com>
2023-04-22 09:05:25 +0900

    [Bronze V] Title: X보다 작은 수, Time: 536 ms, Memory: 31116 KB -BaekjoonHub

---
+++ 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+        int x = sc.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            if(arr[i] < x) {
+                System.out.println(arr[i]);
+            }
+        }
+
+    }
+}
