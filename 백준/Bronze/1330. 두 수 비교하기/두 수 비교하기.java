c295251243cfb535d6344adb5e8ec2877e92118d
Sumin Park <126743151+330sum@users.noreply.github.com>
2023-07-29 14:58:26 +0900

    [Bronze V] Title: 두 수 비교하기, Time: 204 ms, Memory: 17656 KB -BaekjoonHub

--- 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.java
+++ 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.java
@@ -1,20 +1,29 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
 
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        
-        if (a > b) {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String input = scanner.nextLine();
+        String[] split = input.split(" ");
+
+        List<Integer> numbers = new ArrayList<>();
+
+        for (int i = 0; i < split.length; i++) {
+            numbers.add(Integer.parseInt(split[i]));
+        }
+        Integer num1 = numbers.get(0);
+        Integer num2 = numbers.get(1);
+
+        if (num1 > num2) {
             System.out.println(">");
-        } else if (a < b) {
+        } else if (num1 < num2) {
             System.out.println("<");
         } else {
             System.out.println("==");
         }
-
-        sc.close();
     }
 }
