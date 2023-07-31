2eaaa506af3cf6e1cc047ba69acac21fc075ac55
Sumin Park <126743151+330sum@users.noreply.github.com>
2023-07-31 21:31:18 +0900

    [Bronze V] Title: 아스키 코드, Time: 208 ms, Memory: 17712 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        
+        System.out.println((int) new Scanner(System.in).nextLine().charAt(0));
+       
+    }
+}
