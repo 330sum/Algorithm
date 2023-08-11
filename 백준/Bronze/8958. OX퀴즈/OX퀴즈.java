3da5cf9ad4ae555ccc5ca6be3e45deb3b7e9377a
Sumin Park <126743151+330sum@users.noreply.github.com>
2023-08-11 15:32:15 +0900

    [Bronze II] Title: OX퀴즈, Time: 136 ms, Memory: 14164 KB -BaekjoonHub

---
+++ 백준/Bronze/8958. OX퀴즈/OX퀴즈.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < n; i++) {
+            String str = br.readLine();
+            int sum = 0;
+            int count = 0;
+            for (int j = 0; j < str.length(); j++) {
+                if (str.charAt(j) == 'O') {
+                    count++;
+                    sum += count;
+                } else {
+                    count = 0;
+                }
+            }
+            System.out.println(sum);
+        }
+
+
+    }
+}
