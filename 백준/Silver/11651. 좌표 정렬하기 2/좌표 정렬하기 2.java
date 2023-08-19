03a84ddb98ec3fcbbd205b64f96abaa6e8606678
Sumin Park <126743151+330sum@users.noreply.github.com>
2023-08-20 00:12:29 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 2428 ms, Memory: 187188 KB -BaekjoonHub

---
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.java
@@ -0,0 +1,39 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
+class Point implements Comparable<Point> {
+
+    public int x, y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public int compareTo(Point o) {
+        if (this.y == o.y) return this.x - o.x;
+        else return this.y - o.y;
+    }
+}
+
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        List<Point> arr = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            int x = scanner.nextInt();
+            int y = scanner.nextInt();
+            arr.add(new Point(x, y));
+        }
+        Collections.sort(arr);
+        for (Point o : arr) {
+            System.out.println(o.x + " " + o.y);
+        }
+
+    }
+}
