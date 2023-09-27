368a23aaa24da5807b9eb30aa3cdebbf34f3564f
Sumin Park <126743151+330sum@users.noreply.github.com>
2023-09-27 14:55:53 +0900

    [unrated] Title: a와 b 출력하기, Time: 25.54 ms, Memory: 7.41 MB -BaekjoonHub

--- 프로그래머스/unrated/181951. a와 b 출력하기/a와 b 출력하기.py
+++ 프로그래머스/unrated/181951. a와 b 출력하기/a와 b 출력하기.py
@@ -1,6 +1,5 @@
-a, b = map(int, input().strip().split(' '))
-if a >= -100000 and b <= 100000:
-    print('a =', a)
-    print('b =', b)
-else:
-    print('다시 입력해주세요.')
+str = input()
+split = str.split()
+l = list(split)
+print('a = ', l[0])
+print('b = ', l[1])
