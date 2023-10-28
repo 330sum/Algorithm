b76a8eec635c4f9d45668e2ada0db416bcbde012
Sumin Park <126743151+330sum@users.noreply.github.com>
2023-10-28 23:54:26 +0900

    [level unrated] Title: l로 만들기, Time: 3.60 ms, Memory: 10.6 MB -BaekjoonHub

--- 프로그래머스/unrated/181834. l로 만들기/l로 만들기.py
+++ 프로그래머스/unrated/181834. l로 만들기/l로 만들기.py
@@ -1,9 +1,3 @@
 def solution(my_string):
-    answer = ''
-    chars = {'a': 0, 'b': 1, 'c': 2, 'd': 3, 'e': 4, 'f': 5, 'g': 6, 'h': 7, 'i': 8, 'j': 9, 'k': 10, 'l': 11, 'm': 12, 'n': 13, 'o': 14, 'p': 15, 'q': 16, 'r': 17, 's': 18, 't': 19, 'u': 20, 'v': 21, 'w': 22, 'x': 23, 'y': 24,'z': 25}
-
-    for i in my_string:
-        if chars[i] < 11:
-            i = 'l'
-        answer += i
-    return answer
+    answer = [i if i > 'l' else 'l' for i in my_string]
+    return ''.join(answer)
