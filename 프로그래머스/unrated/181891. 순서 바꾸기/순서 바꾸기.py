d597c08f3063a0ed4c2afd9576f47d05b7f4d2cb
Sumin Park <126743151+330sum@users.noreply.github.com>
2023-11-01 14:23:48 +0900

    [level unrated] Title: 순서 바꾸기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/unrated/181891. 순서 바꾸기/순서 바꾸기.py
+++ 프로그래머스/unrated/181891. 순서 바꾸기/순서 바꾸기.py
@@ -1,9 +1,2 @@
 def solution(num_list, n):
-    answer = []
-    length = len(num_list)
-    
-    for i in range(n, length):
-        answer.append(num_list[i])
-    for i in range(0, n):
-        answer.append(num_list[i])
-    return answer
+    return num_list[n:] + num_list[:n]
