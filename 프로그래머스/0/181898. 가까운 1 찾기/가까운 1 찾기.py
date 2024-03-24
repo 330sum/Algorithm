09a3da1d4791e7b69cf42f88898058ec9b1aa9dd
Sumin Park <126743151+330sum@users.noreply.github.com>
2024-03-24 22:18:44 +0900

    [level 0] Title: 가까운 1 찾기, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/181898. 가까운 1 찾기/가까운 1 찾기.py
+++ 프로그래머스/0/181898. 가까운 1 찾기/가까운 1 찾기.py
@@ -1,8 +1,8 @@
 def solution(arr, idx):
-    for i in arr[idx:]:
-        if (i == 1):
-            for j in range(len(arr[idx:])):
-                if arr[idx:][j] == 1:
-                    return j + idx
-                    break
-    return -1
+    answer = 0
+    try:
+        answer = arr.index(1, idx)
+    except:
+        answer = -1
+
+    return answer
